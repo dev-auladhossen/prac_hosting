fd96ec0d9966aa69d3e9f01e97122f204088790d
dev-auladhossen <auladhossen3021@gmail.com>
2025-04-05 13:46:11 +0600

    readme changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -38,6 +44,11 @@ header {
     margin: 0 2rem 0 0;
   }
 
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
   header .wrapper {
     display: flex;
     place-items: flex-start;

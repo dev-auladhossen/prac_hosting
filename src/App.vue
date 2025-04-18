4d6b77766045552cdf2447036c472fcb8bb05421
dev-auladhossen <auladhossen3021@gmail.com>
2025-04-18 23:33:00 +0600

    readme changes

--- src/App.vue
+++ src/App.vue
@@ -489,11 +489,7 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+
   header .wrapper {
     display: flex;
     place-items: flex-start;
